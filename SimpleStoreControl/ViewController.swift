9e216db7d55970004cda148f0dc26e9da8549de9
Elihu <ea_2305@hotmail.com>
2017-02-18 15:14:14 -0600

    Initial Commit

---
+++ SimpleStoreControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleStoreControl
+//
+//  Created by Elihu Alejandro Cruz Albores on 2/18/17.
+//  Copyright © 2017 ObjectiveCTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
